58b07b62ec7097ddb9cd63bd0b85c0ef652adf25
RAHUL RAMAN <58595285+rahul7070@users.noreply.github.com>
2023-06-23 14:56:36 +0530

    frontend_setup

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,26 @@ app.use(cookieParser())
 app.use("/users", userRouter)
 app.use("/question", questionRouter)
 
+app.post("/completion",async (req, res)=>{
+    const options = {
+        method: "POST",
+        headers:{
+            "Authorization": `Bearer ${process.env.API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            message: [{role: "user", content:"how are you"}],
+            max_tokens:100
+        })
+    }
+    try {
+        fetch(`https://api.openai.com/v1/chat/completions`, options)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 app.listen(process.env.PORT, async ()=>{
     try {
         await connection;
